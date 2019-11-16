f2f4e418b784b58815f88eb350bea0cc9cc520a7
Robert Keller <Kellerwrobert@gmail.com>
2019-11-16 18:01:31 -0500

    Built general structure of login component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Login from './components/login';
 import './App.css';
 
 function App() {
-	return <div />;
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<Login
+						formStyle={{
+							backgroundColor: 'rgba(0, 51, 102, .55)',
+							padding: '10px 35px 60px 35px',
+							color: '#BEBEBE',
+							marginTop: 10,
+							width: '80%'
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
