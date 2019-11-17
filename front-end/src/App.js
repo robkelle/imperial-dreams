059fe612e56e9ab03b0f16cf446962a8d4b0e867
Robert Keller <Kellerwrobert@gmail.com>
2019-11-16 19:49:03 -0500

    Completed login UI

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,20 +2,27 @@ import React from 'react';
 import Login from './components/login';
 import './App.css';
 
+const classes = {
+	formStyle: {
+		backgroundColor: 'rgba(0, 51, 102, .55)',
+		padding: '10px 35px 60px 35px',
+		color: '#BEBEBE',
+		marginTop: 10,
+		width: '65%',
+		fontSize: 16
+	},
+	hrStyle: {
+		borderTop: '1px solid #fff',
+		color: '#fff'
+	}
+};
+
 function App() {
 	return (
 		<div className="container">
 			<div className="row">
 				<div className="col">
-					<Login
-						formStyle={{
-							backgroundColor: 'rgba(0, 51, 102, .55)',
-							padding: '10px 35px 60px 35px',
-							color: '#BEBEBE',
-							marginTop: 10,
-							width: '80%'
-						}}
-					/>
+					<Login formStyle={classes.formStyle} hrStyle={classes.hrStyle} />
 				</div>
 			</div>
 		</div>
