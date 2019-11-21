e23f9dcad8361aac5a956c8ff070d45a5a5d2b9d
Robert Keller <Kellerwrobert@gmail.com>
2019-11-20 23:29:05 -0500

    Updated routes to include (info in description)
    Included:
    1. forgotPassword.js
    2. forgotUsername.js
    3. register.js
    
    AND linked all routes and links to their designated pages.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import favicon from './images/favicon.ico';
+import './App.css';
+import ForgotPassword from './components/forgotPassword';
+import ForgotUsername from './components/forgotUsername';
 import Login from './components/login';
 import Landing from './components/landing';
-import { HashRouter, Route, Link } from 'react-router-dom';
-import favicon from './images/favicon.ico';
-
-import './App.css';
+import Register from './components/register';
 
 function App() {
 	return (
 		<div>
-			<HashRouter>
+			<Router basename="imperial">
 				<nav className="navbar navbar-expand-lg" id="custom-nav" style={{ backgroundColor: 'rgb(0, 51, 102)' }}>
 					<a className="navbar-brand" href="/">
 						<img src={favicon} height={35} alt="" />
@@ -28,10 +30,14 @@ function App() {
 					</ul>
 				</nav>
 				<div align="center">
+					{/* Specify all routes in the client-side */}
 					<Route exact path="/" component={Landing} />
 					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/forgot_username" component={ForgotUsername} />
+					<Route exact path="/forgot_password" component={ForgotPassword} />
 				</div>
-			</HashRouter>
+			</Router>
 		</div>
 	);
 }
