59db88a49729c30ea9fdc2eafab4f79d18ea56cb
Robert Keller <Kellerwrobert@gmail.com>
2019-12-03 22:54:30 -0500

    Added chat component and added some more featurs to the chat component
    1. Added auto scrolling
    2. Added message send on enter

--- front-end/src/components/userDashboard.js
+++ front-end/src/components/userDashboard.js
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
-
 import BorderBackground from '../images/borderBackground.jpg';
-import config from '../config.json';
-import io from 'socket.io-client';
-import { withCookies } from 'react-cookie';
+import Chat from './chat';
+import React from 'react';
 
 const classes = {
 	row: {
@@ -23,117 +20,34 @@ const classes = {
 	header: {
 		position: 'absolute',
 		left: 45,
-		top: 6,
+		top: 0,
 		fontFamily: 'Trade Winds',
 		color: '#E6E8EA'
 	}
 };
 
-// Initiate socket.io
-const socket = io('http://localhost:4000');
-
-class UserDashboard extends Component {
-	state = {
-		announcements: ''
-	};
-	componentDidMount() {
-		const { cookies } = this.props;
-		fetch(`${config.API.DOMAIN}:${config.API.PORT}/api/user/verify`, {
-			method: 'GET',
-			mode: 'cors',
-			credentials: 'include',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-			.then((res) => {
-				return res.json();
-			})
-			.then((res) => {
-				if (res.httpStatus === 401) {
-					cookies.remove('isAuthorized', { path: '/' });
-				}
-			});
-
-		socket.emit('load', 'load');
-		socket.on('refresh', (res) => {
-			this.setState({
-				announcements: res.message.map((value, index) => (
-					<div key={index}>
-						<strong>{value.username}: </strong>
-						{value.message}
-					</div>
-				))
-			});
-		});
-	}
-
-	addMessage = (message) => {
-		const { cookies } = this.props;
-		socket.emit('addMessage', { message: message, username: cookies.get('user') });
-		this.setState({ message: '' });
-
-		socket.on('refresh', (res) => {
-			this.setState({
-				announcements: res.message.map((value, index) => (
-					<div key={index}>
-						<strong>{value.username}: </strong>
-						{value.message}
-					</div>
-				))
-			});
-		});
-	};
-
-	render() {
-		return (
-			<div
-				className="container"
-				style={{
-					backgroundColor: 'rgb(0, 51, 102)',
-					padding: 25,
-					marginTop: 10,
-					borderImage: `url(${BorderBackground}) 30 round`,
-					borderWidth: '25px',
-					borderStyle: 'solid'
-				}}
-			>
-				<div className="row" style={classes.row}>
-					<div className="col-11" style={classes.colStyle}>
-						<h1 style={classes.header}>CHAT</h1>
-
-						<div style={{ backgroundColor: '#485563', height: 50 }} />
-						<div className="overflow-auto side_bar" style={{ overflow: 'visible', height: 300 }}>
-							<div style={{ padding: 20, marginBottom: 30 }}>{this.state.announcements}</div>
-						</div>
-
-						<div className="input-group">
-							<div className="input-group-prepend">
-								<button
-									className="btn btn-outline-secondary"
-									type="button"
-									onClick={() => this.addMessage(this.state.message)}
-								>
-									Post
-								</button>
-							</div>
-							<input
-								type="text"
-								className="form-control"
-								placeholder=""
-								aria-label=""
-								aria-describedby="basic-addon1"
-								value={this.state.message || ''}
-								onChange={(e) => {
-									this.setState({ message: e.target.value });
-								}}
-							/>
-						</div>
-					</div>
+const UserDashboard = () => {
+	return (
+		<div
+			className="container"
+			style={{
+				backgroundColor: 'rgb(0, 51, 102)',
+				padding: 25,
+				marginTop: 10,
+				borderImage: `url(${BorderBackground}) 30 round`,
+				borderWidth: '25px',
+				borderStyle: 'solid'
+			}}
+		>
+			<div className="row" style={classes.row}>
+				<div className="col-11" style={classes.colStyle}>
+					<h1 style={classes.header}>CHAT</h1>
+					<div style={{ backgroundColor: '#485563', height: 50 }} />
+					<Chat />
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
-export default withCookies(UserDashboard);
+export default UserDashboard;
