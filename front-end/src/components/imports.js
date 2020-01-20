9b08f8375be3c758c2f4bc359838169f28061113
Robert Keller <Kellerwrobert@gmail.com>
2020-01-20 17:42:53 -0500

    File name changes and separated components out of chat.js

--- front-end/src/components/imports.js
+++ front-end/src/components/imports.js
@@ -4,15 +4,15 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import BorderBackground from '../images/borderBackground.jpg';
 import ButtonBackground from '../images/buttonBackground.jpg';
-import { ForgotPassword } from './Authorization/forgotPassword';
-import { Landing } from './landing';
-import { LoadingScreen } from './loadingScreen';
-import Login from './Authorization/login';
-import { Navbar } from './navbar';
+import { ForgotPassword } from './Authorization/ForgotPassword';
+import { Landing } from './Landing';
+import { LoadingScreen } from './LoadingScreen';
+import Login from './Authorization/Login';
+import { Navbar } from './Navbar';
 import React from 'react';
-import { Register } from './Authorization/register';
-import { ResetPassword } from './Authorization/resetPassword';
-import UserDashboard from './userDashboard';
+import { Register } from './Authorization/Register';
+import { ResetPassword } from './Authorization/ResetPassword';
+import UserDashboard from './UserDashboard';
 import { withCookies } from 'react-cookie';
 
 export {
