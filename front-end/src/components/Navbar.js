b27d57960bd924a50f9ec71babcb37bf6bf18e3a
Robert Keller <Kellerwrobert@gmail.com>
2020-01-20 23:18:25 -0500

    Added AppBar material-ui component, removed bootstrap styling

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,47 +1,62 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import Favicon from '../images/favicon.ico';
+import IconButton from '@material-ui/core/IconButton';
 import { Link } from 'react-router-dom';
 import Logout from './Authorization/Logout';
+import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1
+	},
+	menuButton: {
+		marginRight: theme.spacing(2)
+	},
+	title: {
+		flexGrow: 1
+	}
+}));
 
 const Navbar = (props) => {
 	const { isAuthorized } = props;
+	const classes = useStyles();
 	return (
-		<nav className="navbar navbar-expand-lg" id="custom-nav" style={{ backgroundColor: 'rgb(0, 51, 102)' }}>
-			<a className="navbar-brand" href="/imperial">
-				<img src={Favicon} height={35} alt="" />
-			</a>
-			<ul className="navbar-nav">
-				<li className="nav-item active">
-					<div className="nav-link" href="/">
-						<Link to="/">Home</Link>
-					</div>
-				</li>
-				{!isAuthorized ? (
-					<li className="nav-item">
-						<div className="nav-link" href="/login">
+		<div className={classes.root}>
+			<AppBar position="static" style={{ backgroundColor: '#181818' }}>
+				<Toolbar>
+					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						<Button color="inherit">
+							<Link to="/">Imperial Dreams</Link>
+						</Button>
+					</Typography>
+
+					{!isAuthorized ? (
+						<Button color="inherit">
 							<Link to="/login">Login</Link>
-						</div>
-					</li>
-				) : (
-					<li className="nav-item">
-						<div className="nav-link" href="/login">
+						</Button>
+					) : (
+						<Button color="inherit">
 							<Link to="/user_dashboard">Dashboard</Link>
-						</div>
-					</li>
-				)}
-			</ul>
-			{isAuthorized ? (
-				<ul className="nav navbar-nav ml-auto">
-					<li className="nav-item">
-						<div className="nav-link" href="/login">
+						</Button>
+					)}
+					{isAuthorized ? (
+						<Button color="inherit">
 							<Logout />
-						</div>
-					</li>
-				</ul>
-			) : (
-				''
-			)}
-		</nav>
+						</Button>
+					) : (
+						''
+					)}
+				</Toolbar>
+			</AppBar>
+		</div>
 	);
 };
 
