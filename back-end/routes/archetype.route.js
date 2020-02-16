90d50d1d6392ff7b2878ddfdda8c413ae2a14333
Robert Keller <Kellerwrobert@gmail.com>
2020-02-16 16:06:37 -0500

    Update archetype.route.js

--- back-end/routes/archetype.route.js
+++ back-end/routes/archetype.route.js
@@ -2,6 +2,7 @@ import archetypeController from '../controllers/archetype.controller';
 import express from 'express';
 const router = express.Router();
 
+// Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.
 const multer = require('multer');
 
 const upload = multer({
