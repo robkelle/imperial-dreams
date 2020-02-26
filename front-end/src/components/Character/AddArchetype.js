7c4e9cf354640768d1924795803fa9f48a6a4dc5
Robert Keller <Kellerwrobert@gmail.com>
2020-02-25 22:35:37 -0500

    Update AddArchetype.js

--- front-end/src/components/Character/AddArchetype.js
+++ front-end/src/components/Character/AddArchetype.js
@@ -1,4 +1,4 @@
-import { Button, FormControl, Input, InputLabel, Paper } from '@material-ui/core';
+import { Button, FormControl, Grid, Input, InputLabel, Paper } from '@material-ui/core';
 import React, { useState } from 'react';
 
 import config from '../../config.json';
@@ -8,8 +8,8 @@ const AddArchetype = () => {
 		e.preventDefault();
 		let formData = new FormData();
 		formData.append('archetypeImage', file);
-		formData.append('type', 'hairColor');
-		formData.append('label', 'brown');
+		formData.append('type', type);
+		formData.append('label', label);
 
 		fetch(`${config.API.DOMAIN}:${config.API.PORT}/api/archetype`, {
 			method: 'POST',
@@ -24,37 +24,51 @@ const AddArchetype = () => {
 	};
 
 	const [ file, setFile ] = useState();
+	const [ type, setType ] = useState();
+	const [ label, setLabel ] = useState();
 
 	return (
-		<Paper>
-			<form>
-				<FormControl>
-					<InputLabel id="type">Type</InputLabel>
-					<Input />
-				</FormControl>
-				<FormControl>
-					<InputLabel type="label">Label</InputLabel>
-					<Input />
-				</FormControl>
-				<FormControl>
-					<Button variant="contained" component="label">
-						Upload File
-						<input
-							type="file"
-							style={{ display: 'none' }}
-							onChange={(e) => {
-								setFile(e.target.files[0]);
-							}}
-						/>
-					</Button>
-				</FormControl>
-				<FormControl>
-					<Button variant="contained" type="submit" onClick={(e) => handleSubmit(e)}>
-						Submit
-					</Button>
-				</FormControl>
-			</form>
-		</Paper>
+		<div width="100%" style={{ marginTop: 20 }}>
+			<Grid container justify="center">
+				<Grid item>
+					<Paper style={{ padding: 10 }}>
+						<form>
+							<div>
+								<FormControl>
+									<InputLabel id="type">Type</InputLabel>
+									<Input onChange={(e) => setType(e.target.value)} />
+								</FormControl>
+							</div>
+							<div>
+								<FormControl>
+									<InputLabel type="label">Label</InputLabel>
+									<Input onChange={(e) => setLabel(e.target.value)} />
+								</FormControl>
+							</div>
+							<br />
+							<FormControl>
+								<Button variant="contained" component="label">
+									Upload File
+									<input
+										type="file"
+										style={{ display: 'none' }}
+										onChange={(e) => {
+											setFile(e.target.files[0]);
+										}}
+									/>
+								</Button>
+							</FormControl>
+
+							<FormControl>
+								<Button variant="contained" type="submit" onClick={(e) => handleSubmit(e)}>
+									Submit
+								</Button>
+							</FormControl>
+						</form>
+					</Paper>
+				</Grid>
+			</Grid>
+		</div>
 	);
 };
 
