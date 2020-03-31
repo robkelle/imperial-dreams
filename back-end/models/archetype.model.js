5ec2d5d8327440cbcd3f91d9392c4f8ddfc4f197
Robert Keller <Kellerwrobert@gmail.com>
2020-03-31 08:28:11 -0400

    Updates

--- back-end/models/archetype.model.js
+++ back-end/models/archetype.model.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
+
 const Schema = mongoose.Schema;
 
 // Fix deprecation warnings
 mongoose.set('useFindAndModify', false);
-
-// https://www.bryndonovan.com/2015/06/16/master-list-of-physical-descriptions/
 
 module.exports = mongoose.model(
 	'archetypes',
---
+++ back-end/models/character.model.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+// Fix deprecation warnings
+mongoose.set('useFindAndModify', false);
+
+module.exports = mongoose.model('character', new Schema({}));
