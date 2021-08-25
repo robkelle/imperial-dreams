b466e80c8896e7bbb43d1e01991825ab44c6e1dc
Rob Keller <34144509+robkelle@users.noreply.github.com>
2021-08-25 10:16:10 -0400

    Updates to inventory design

--- front-end/src/components/Inventory/Inventory.js
+++ front-end/src/components/Inventory/Inventory.js
@@ -8,77 +8,172 @@ import materials from '../../images/BotW_Material_Icon.png';
 import shield from '../../images/BotW_Shield_Icon.png';
 import weapon from '../../images/BotW_Weapon_Icon.png';
 
-//import rare from '../../images/BotW_Key_Item_Icon.png';
-
-
-
 const inventory = [
 	{
-		type: 'Material',
-		name: 'Mushroom',
-		quantity: 5,
-		image: ''
-	},
-	{
-		type: 'Material',
-		name: 'Apple',
-		quantity: 1,
-		image: ''
-	},
-	{
-		type: 'Material',
-		name: 'Gold',
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Diamond Sword',
 		quantity: 2,
 		image: ''
 	},
 	{
-		type: 'Material',
-		name: 'Wood',
-		quantity: 25,
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Gold Sword',
+		quantity: 5,
 		image: ''
 	},
 	{
-		type: 'Material',
-		name: 'Steel',
-		quantity: 27,
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Diamond Sword',
+		quantity: 2,
 		image: ''
 	},
 	{
-		type: 'Material',
-		name: 'Diamond',
-		quantity: 1,
-		image: 'ALttP_Bow_&_Arrows_Sprite.png'
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Gold Sword',
+		quantity: 5,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Diamond Sword',
+		quantity: 2,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Gold Sword',
+		quantity: 5,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Diamond Sword',
+		quantity: 2,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Gold Sword',
+		quantity: 5,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Diamond Sword',
+		quantity: 2,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Gold Sword',
+		quantity: 5,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Diamond Sword',
+		quantity: 2,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Gold Sword',
+		quantity: 5,
+		image: ''
+	},
+	{
+		type: {
+			name: 'Weapon',
+			index: 0
+		},
+		name: 'Gold Sword',
+		quantity: 5,
+		image: ''
 	}
 ];
 
 export class Inventory extends Component {
+	constructor() {
+		super();
+		this.state = {
+			tabValue: 0
+		};
+
+		this.handleChange = (e, newValue) => {
+			this.setState({ tabValue: newValue });
+		};
+	}
+
 	render() {
+		const { tabValue } = this.state;
 		return (
 			<Fragment>
-				<div className="animate__animated animate__backInDown">
-					<Tabs indicatorColor="primary" textColor="inherit" centered={true}>
-						<Tab icon={<Avatar src={weapon} />} />
-						<Tab icon={<Avatar src={bow} />} />
-						<Tab icon={<Avatar src={shield} />} />
-						<Tab icon={<Avatar src={armor} />} />
-						<Tab icon={<Avatar src={materials} />} />
-						<Tab icon={<Avatar src={food} />} />
-					</Tabs>
+				<Tabs centered={true} value={tabValue} onChange={this.handleChange} style={{backgroundColor: 'rgba(24, 24, 24, .75)', paddingBottom: 10}}>
+					<Tab value={0} icon={<Avatar src={weapon} />} />
+					<Tab value={1} icon={<Avatar src={bow} />} />
+					<Tab value={2} icon={<Avatar src={shield} />} />
+					<Tab value={3} icon={<Avatar src={armor} />} />
+					<Tab value={4} icon={<Avatar src={materials} />} />
+					<Tab value={5} icon={<Avatar src={food} />} />
+				</Tabs>
 
-					<Grid container spacing={0} justify="center">
+				<div align="center" style={{marginTop: 10}}>
+					<Grid container lg={11} spacing={1}>
 						{inventory.map((value, index) => {
-							return (
-								<Grid lg={1} item style={{ margin: 5 }} key={index}>
-									<Card style={{ backgroundColor: 'rgba(24,24,24, .75)', color: '#fff' }}>
-										<CardContent>
-											<Typography variant="overline">{value.name}</Typography>
-										</CardContent>
-										<CardActions>
-											<Typography variant="subtitle2">x {value.quantity}</Typography>
-										</CardActions>
-									</Card>
-								</Grid>
-							);
+							if (value.type.index === tabValue) {
+								return (
+									<Grid item lg={1} key={index} spacing={5} alignContent="space-between">
+										<div className="animate__animated animate__backInDown">
+											<Card className="texture2">
+												<CardContent>
+													<Typography variant="overline">{value.name}</Typography>
+												</CardContent>
+												<CardActions>
+													<Typography variant="subtitle2">x {value.quantity}</Typography>
+												</CardActions>
+											</Card>
+										</div>
+									</Grid>
+								);
+							} else {
+								return <div key={index} />;
+							}
 						})}
 					</Grid>
 				</div>
