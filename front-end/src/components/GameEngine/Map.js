203e04a572fa8ef1ed10d32df4724378246cd325
Rob Keller <34144509+robkelle@users.noreply.github.com>
2021-09-09 19:28:19 -0400

    Implemented viewport on map

--- front-end/src/components/GameEngine/Map.js
+++ front-end/src/components/GameEngine/Map.js
@@ -2,13 +2,13 @@ import * as PIXI from 'pixi.js';
 
 import React, { Component } from 'react';
 
+import { Viewport } from 'pixi-viewport';
+
 export class Map extends Component {
 	constructor() {
 		super();
 		this.state = {
-			app: new PIXI.Application(),
-			graphics: new PIXI.Graphics(),
-			container: new PIXI.Container(),
+			app: new PIXI.Application({ resizeTo: window }),
 			playerSheet: {}
 		};
 
@@ -23,21 +23,22 @@ export class Map extends Component {
 		this.keys[e.keyCode] = true;
 	};
 
-	doneLoading = (app) => {
+	doneLoading = (app, viewport) => {
 		this.createPlayerSheet(app);
-		this.createPlayer(app);
+		this.createPlayer(app, viewport);
 		app.ticker.add(() => {
-			this.gameLoop();
+			this.gameLoop(viewport);
 		});
 	};
 
 	createPlayerSheet = (app) => {
 		let ps = this.state;
+		let w = 56;
+		let h = 84;
+
 		this.setState({
 			ssheet: new PIXI.BaseTexture.from(app.loader.resources['king'].url)
 		});
-		let w = 56;
-		let h = 84;
 
 		ps.playerSheet['standSouth'] = [
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(0 * w, 0, w, h))
@@ -46,29 +47,34 @@ export class Map extends Component {
 		ps.playerSheet['walkNorth'] = [
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(115, 87, w, h)),
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(114, 1, w, h)),
-			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(1, 173, w, h))
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(1, 173, w, h)),
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(115, 87, w, h))
 		];
 
 		ps.playerSheet['walkEast'] = [
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(58, 87, w, h)),
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(58, 173, w, h)),
-			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(228, 87, w, h))
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(228, 87, w, h)),
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(58, 87, w, h))
 		];
 
 		ps.playerSheet['walkSouth'] = [
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(1, 1, w, h)),
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(171, 1, w, h)),
-			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(172, 87, w, h))
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(172, 87, w, h)),
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(1, 1, w, h))
 		];
 
 		ps.playerSheet['walkWest'] = [
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(57, 1, w, h)),
 			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(227, 1, w, h)),
-			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(1, 87, w, h))
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(1, 87, w, h)),
+			new PIXI.Texture(this.state.ssheet, new PIXI.Rectangle(57, 1, w, h))
 		];
 	};
 
-	createPlayer = (app) => {
+	createPlayer = (app, viewport) => {
+		// Sets the default stance of the player to standing South
 		this.setState({
 			player: new PIXI.AnimatedSprite(this.state.playerSheet.standSouth)
 		});
@@ -76,15 +82,17 @@ export class Map extends Component {
 		let player = this.state.player;
 
 		player.anchor.set(0.5);
-		player.animationSpeed = 1.5;
+		player.animationSpeed = .5;
 		player.loop = false;
 		player.x = app.view.width / 2;
 		player.y = app.view.height / 2;
-		app.stage.addChild(player);
+		player.zIndex = 1;
+		viewport.addChild(player);
+		viewport.follow(player);
 		player.play();
 	};
 
-	gameLoop = () => {
+	gameLoop = (viewport) => {
 		let player = this.state.player;
 
 		// W Key
@@ -128,39 +136,54 @@ export class Map extends Component {
 		}
 	};
 
+	// Life Cycle Components
+	componentWillUnmount() {
+		// Removes caching when component is unmounted
+		new PIXI.utils.clearTextureCache();
+	}
+
 	componentDidMount() {
 		if (!this.props.cookies.cookies.isAuthorized) {
 			this.props.history.push('/login');
 		}
 
 		let app = this.state.app;
-		let container = this.state.container;
+
+		// Draw the PIXI canvas
+		document.body.appendChild(app.view);
+
+		// Create viewport
+		const viewport = new Viewport({
+			screenWidth: window.innerWidth,
+			screenHeight: window.innerHeight,
+			worldWidth: 1000,
+			worldHeight: 1000,
+
+			interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+		});
 
 		app.renderer.view.style.position = 'fixed';
 		app.renderer.view.style.display = 'block';
-		app.renderer.autoResize = true;
-		app.renderer.resize(window.innerWidth, window.innerHeight);
+		// The root display container that's rendered
+		app.stage.addChild(viewport);
 
-		let canvas = document.getElementById('pixi-canvas');
+		// Activate plugins
+		viewport.drag().pinch().wheel().decelerate();
+		viewport.mouseEdges();
 
-		// Draw the PIXI canvas
-		canvas.appendChild(app.view);
-		app.stage.addChild(container);
-
-		// Create a new texture
+		// Creates player texture
 		app.loader.add('king', require('../../images/characterSprite.png'));
-		app.loader.load(() => {
-			this.doneLoading(app);
-		});
 
 		window.addEventListener('keydown', this.keysDown);
 		window.addEventListener('keyup', this.keysUp);
 
-		//app.ticker.add(() => this.gameLoop(player));
+		app.loader.load(() => {
+			this.doneLoading(app, viewport);
+		});
 	}
 
 	render() {
-		return <div id='pixi-canvas' />;
+		return <div />;
 	}
 }
 
